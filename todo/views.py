4c288630b2409166c88372ecbda07e0df69130e3
Luka Guledani <lukaguledani123@gmail.com>
2024-07-18 18:15:30 +0400

    added queriset for database engagement

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Task
+
 # Create your views here.
 def todo_index(request):
-    return render(request, 'todo_list.html')     
+    queriset = Task.objects.all()
+    return render(request, 'todo_list.html',{'data': queriset})     
