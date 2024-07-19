84b6b65f097e14cbc0cdf0f4260135c340f9ede1
Luka Guledani <lukaguledani123@gmail.com>
2024-07-19 17:59:00 +0400

    created new path

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import todo_index
+from .views import todo_index, add_task
 
 urlpatterns = [ 
-    path('', todo_index) 
+    path('', todo_index, name = 'todo_list'), 
+    path('add/', add_task , name = 'add_task')
 ]     
