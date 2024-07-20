fb0c9e18b56bb072a7d1e6a07bf2e73ed7f4c500
Luka Guledani <lukaguledani123@gmail.com>
2024-07-20 19:38:14 +0400

    added path

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import todo_index, add_task
+from .views import todo_index, add_task, delete_task, edit_task
 
 urlpatterns = [ 
     path('', todo_index, name = 'todo_list'), 
-    path('add/', add_task , name = 'add_task')
-]     
+    path('add/', add_task , name = 'add_task'),
+    path('delete/<int:task_id>', delete_task, name = 'delete_task'),
+    path('edit/<int:task_id>', edit_task, name = 'edit_task'),
+]      
