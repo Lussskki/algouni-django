a5095e02a6aa8be1e30002b6a2cba66e96471a10
Luka Guledani <lukaguledani123@gmail.com>
2024-07-20 19:39:32 +0400

    created delete and edit function

--- todo/views.py
+++ todo/views.py
@@ -10,5 +10,15 @@ def todo_index(request):
 def add_task(request):
     if request.method == "POST":
         title = request.POST.get("title")
-        Task.objects.create(title = title)
+        Task.objects.create(title = title) 
     return redirect("todo_list")    
+
+def delete_task(request, task_id):
+    Task.objects.filter(pk = task_id).delete()
+    return redirect("todo_list")
+
+def edit_task(request, task_id):
+    task = Task.objects.get(pk = task_id)
+    task.completed = True
+    task.save()
+    return redirect("todo_list")
